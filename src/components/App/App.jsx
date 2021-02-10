c87a77de29f4b19212dfe41ae9e18c491392ba03
gregorykafanov <gregkaf@gmail.com>
2021-02-10 18:09:18 +0200

    (feat): add background

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,21 @@
 import './App.css'
+// import backgroundImage from '../../assets/images/background.png'
+
+// const gameStyles = {
+//   height: '100vh',
+//   backgroundImage: `url(${backgroundImage})`,
+//   backgroundSize: 'cover',
+//   backgroundPosition: 'center',
+//   position: 'relative',
+//   overflow: 'hidden',
+// }
 
 function App() {
-  return <div className="App">Hello</div>
+  return (
+    <div className="game">
+      <div className="inner"></div>
+    </div>
+  )
 }
 
 export default App
